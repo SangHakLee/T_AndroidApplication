63648a8d7138a77aaaeaf5d7a1999c80f9f403bc
unknown <sanghaklee>
2016-01-19 10:07:02 +0900

    View 상속관계. View 속성 테스트. Inflate로 View 그리기 3가지 테스트

--- ViewProject/app/src/main/java/com/example/viewproject/MainActivity.java
+++ ViewProject/app/src/main/java/com/example/viewproject/MainActivity.java
@@ -1,13 +1,43 @@
 package com.example.viewproject;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        // inflation 직접 생성하기
+        // 1. 직접 생성
+//        LinearLayout view = new LinearLayout(this); // context 객체 무조건 필요 여기선 this
+//        view.setBackgroundColor(Color.RED);
+//        Button btn = new Button(this); // 여기서도 context 객체 필요
+//        btn.setText("New Button");
+//
+//        view.addView(btn);
+//        setContentView(view);
+        //
+
+        // 2. inflater
+//        LayoutInflater inflater =(LayoutInflater)getSystemService(LAYOUT_INFLATER_SERVICE);
+//        LinearLayout view =  (LinearLayout)inflater.inflate(R.layout.sam, null); // 명시적 캐스팅
+////        View view =  inflater.inflate(R.layout.sam, null); // 묵시적 캐스팅
+//        setContentView(view);
+
+        // 3.
+        View view = View.inflate(this, R.layout.sam, null);
+        setContentView(view);
+
+
+
+
+
+
+
+
+
+//        setContentView(R.layout.activity_main);
     }
 }
