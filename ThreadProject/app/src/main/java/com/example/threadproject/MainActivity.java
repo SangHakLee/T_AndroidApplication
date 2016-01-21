af0e50b2c74a7fbf8b2cd5b7237e2d15bd41bcff
unknown <sanghaklee>
2016-01-21 17:47:06 +0900

    쓰레드와 핸들러로 UI 변경하기

--- ThreadProject/app/src/main/java/com/example/threadproject/MainActivity.java
+++ ThreadProject/app/src/main/java/com/example/threadproject/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.threadproject;
 
+import android.os.Handler;
+import android.os.Message;
 import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -24,13 +26,30 @@ public class MainActivity extends AppCompatActivity {
     };
 
     boolean onAir = false;
+    Handler handler1 = new Handler(){//  android.os 에 있는 Handler import
+        @Override
+        public void handleMessage(Message msg) {
+            et.setText(msg.arg1+""); // 여기서 sendMessage로 받은 메시지를 받아온다.
+        }
+    };
 
     class AnyThread extends Thread{
         public void run(){
+
+            Message msg = null; // 여기서 만들면 에러. 재사용 하면 안된다.
             while(onAir){
                 cnt++;
                 Log.v(TAG, "cnt : "+ cnt);
-//                et.setText("cnt : " + cnt); // 내가 만든 쓰레드는 UI 변경 불가. 메인 쓰레드만 가능하다. Toast도 마찬가지 불가능
+
+                // 핸들러를 이용해서 메세지 보내기
+                msg = handler1.obtainMessage();
+
+                msg.arg1 = cnt;
+
+                handler1.sendMessage(msg); // handleMessage로 간다.
+
+
+//                et.setText("cnt : " + cnt); // 내가 만든 쓰레드는 UI 변경 불가. 메인 쓰레드만 가능하다. Toast도 마찬가지 불가능  -> Handler 필요
                 SystemClock.sleep(300);
             }
         }
