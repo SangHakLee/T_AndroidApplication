78aea9ce30655c92a3598302b6c0498922376d7e
unknown <sanghaklee>
2016-01-21 17:32:54 +0900

    쓰레드 사용시 주의사항. 내가 만든 쓰레드로드 UI 변경이 불가능하다.

--- ThreadProject/app/src/main/java/com/example/threadproject/MainActivity.java
+++ ThreadProject/app/src/main/java/com/example/threadproject/MainActivity.java
@@ -1,14 +1,15 @@
 package com.example.threadproject;
 
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "Main";
-
     EditText et;
 
     View.OnClickListener handler = new View.OnClickListener() {
@@ -22,13 +23,27 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+    boolean onAir = false;
+
+    class AnyThread extends Thread{
+        public void run(){
+            while(onAir){
+                cnt++;
+                Log.v(TAG, "cnt : "+ cnt);
+//                et.setText("cnt : " + cnt); // 내가 만든 쓰레드는 UI 변경 불가. 메인 쓰레드만 가능하다. Toast도 마찬가지 불가능
+                SystemClock.sleep(300);
+            }
+        }
+    }
+
     int cnt = 0;
 
     void doAction1(){
         // 시작
         cnt++;
         et.setText("cnt : " + cnt);
-
+        onAir = true;
+        new AnyThread().start(); // 쓰레드 실행
         // 끝 5초 이내 완료가 필수
     }
 
