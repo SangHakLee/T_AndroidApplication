ba21734fa06da254fc98097147b66ce4ca594e01
unknown <sanghaklee>
2016-01-21 09:56:11 +0900

    내가 만든 Fragment에 접근하기. FragmnetManager 이용

--- MyApplication/app/src/main/java/com/example/fragmentproject/FirstFragment.java
+++ MyApplication/app/src/main/java/com/example/fragmentproject/FirstFragment.java
@@ -7,6 +7,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class FirstFragment extends Fragment {
@@ -20,7 +22,7 @@ public class FirstFragment extends Fragment {
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        this.context = context;
+        this.context = context; // attach 될때 현재 context를 넣는다.
     }
 
     @Override
@@ -29,11 +31,31 @@ public class FirstFragment extends Fragment {
         context = null;
     }
 
+    TextView tv;
+    EditText et;
+    View.OnClickListener handler = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            tv.setText(et.getText().toString()); // 에딧텍스트의 값은 tv로
+
+//            switch (v.getId()){
+//                case 1:
+//                break;
+//            }
+        }
+    };
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_first, null, false); // 어떤것 inflate 할건지, 부모 객체 필요없어서 null
 //        return super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_first, null, false); // 어떤것 inflate 할건지, 부모 객체 필요없어서 null
+
+        // view로 접근하면 onClick... r같은거 할수 있다.
+        tv = (TextView)view.findViewById(R.id.showText);
+        et = (EditText)view.findViewById(R.id.editText);
+        view.findViewById(R.id.button).setOnClickListener(handler);
+
         return view; // 부모를 넘기는게 아니라 내가 만든 fragment_first를 inflate 해서 넘긴다.
     }
 }
--- MyApplication/app/src/main/java/com/example/fragmentproject/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/fragmentproject/MainActivity.java
@@ -1,13 +1,22 @@
 package com.example.fragmentproject;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText et;
+    FirstFragment firstFragment; // 프래그먼트에 접근하기 위해서 선언
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        et = (EditText)findViewById(R.id.editText2);
+
+        // 만든 프래그먼트에 접근하기
+        FragmentManager manager = getSupportFragmentManager(); // 어디것을 가져오는지 중요 FragmentManager은 두개가 있다. 우린 support.v4 사용
+        firstFragment = (FirstFragment)manager.findFragmentById(R.id.fragment); // 캐스팅 필요
     }
 }
