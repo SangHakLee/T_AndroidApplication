7f6cc4e8e0f2b2595f0fbde247a501a063583c40
unknown <sanghaklee>
2016-01-21 10:26:17 +0900

    Fragment 두개 만들어서 replace 시킴. Fragment 전환 효과

--- MyApplication/app/src/main/java/com/example/fragmentproject/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/fragmentproject/MainActivity.java
@@ -4,11 +4,47 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     EditText et;
     FirstFragment firstFragment; // 프래그먼트에 접근하기 위해서 선언
+    SecondFragment secondFragment; // 프래그먼트에 접근하기 위해서 선언
+
+    // 핸들러에 넣어주기 위해 멤버변수 밖으로 뺌
+    FragmentManager manager = getSupportFragmentManager(); // 어디것을 가져오는지 중요 FragmentManager은 두개가 있다. 우린 support.v4 사용
+    FragmentTransaction ft = manager.beginTransaction();
+
+
+    View.OnClickListener handler = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.button2 :
+                    doAction1();
+                    break;
+                case R.id.button4 :
+                    doAction2();
+                    break;
+            }
+        }
+    };
+
+    void doAction1(){
+        firstFragment = new FirstFragment();
+        FragmentTransaction ft = manager.beginTransaction();
+        ft.replace(R.id.main, firstFragment); // main위에다 firstFragment로 대체한다.
+        ft.commit();
+    }
+
+    void doAction2(){
+        secondFragment = new SecondFragment();
+        FragmentTransaction ft = manager.beginTransaction();
+        ft.replace(R.id.main, secondFragment); // main위에다 secondFragment 대체한다.
+        ft.commit();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,6 +52,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main1);
 
         et = (EditText)findViewById(R.id.editText2);
+
+        findViewById(R.id.button2).setOnClickListener(handler);
+        findViewById(R.id.button4).setOnClickListener(handler);
 
         // 만든 프래그먼트에 접근하기
         FragmentManager manager = getSupportFragmentManager(); // 어디것을 가져오는지 중요 FragmentManager은 두개가 있다. 우린 support.v4 사용
@@ -26,7 +65,7 @@ public class MainActivity extends AppCompatActivity {
             // 동적 생성
             firstFragment = new FirstFragment();
             FragmentTransaction ft = manager.beginTransaction();
-            ft.add(R.id.main, firstFragment);
+            ft.add(R.id.main, firstFragment); // 있는거 위에 올린다.
             ft.commit();
         }
     }
