98de8926c987875253f3800351ce68e055aa8973
unknown <sanghaklee>
2016-01-21 09:29:51 +0900

    FragmentProject init

---
+++ MyApplication/app/src/main/java/com/example/fragmentproject/FirstFragment.java
@@ -0,0 +1,13 @@
+package com.example.fragmentproject;
+
+import android.support.v4.app.Fragment;
+
+
+public class FirstFragment extends Fragment {
+
+    public FirstFragment() {
+        // Required empty public constructor
+    }
+
+
+}
