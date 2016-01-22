444845ac19321abb2c13477778ebafefb41a8d24
unknown <sanghaklee>
2016-01-22 14:35:08 +0900

    메뉴 나오게 하기 xml, java 둘다 실습

--- MenuProject/app/src/main/java/com/example/menuproject/MainActivity.java
+++ MenuProject/app/src/main/java/com/example/menuproject/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.menuproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu); // 메뉴 Inflate 하기, xml로
+        MenuItem item = menu.add(0, 100, 5,"100 추가" ); // 코드로 추가하기
+        item.setIcon(R.mipmap.ic_launcher);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) { // 실제 메뉴 보여지는 곳
+        switch (item.getItemId()){
+            case R.id.menu1 :
+                Toast.makeText(this, item.getTitle(), Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu2 :
+                Toast.makeText(this, item.getTitle(), Toast.LENGTH_SHORT).show();
+                break;
+            case 100 :
+                Toast.makeText(this, item.getTitle(), Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
