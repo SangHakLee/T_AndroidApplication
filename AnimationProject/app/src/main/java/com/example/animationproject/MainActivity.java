24e01b93d9d87743d44f489ff6ba37af71aa6efd
unknown <sanghaklee>
2016-01-22 13:16:55 +0900

    TextSwitcher 로 텍스트 전환.

--- AnimationProject/app/src/main/java/com/example/animationproject/MainActivity.java
+++ AnimationProject/app/src/main/java/com/example/animationproject/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.animationproject;
 
-import android.animation.AnimatorInflater;
+import android.animation.ArgbEvaluator;
+import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
+import android.graphics.Color;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -10,6 +12,7 @@ import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.TextSwitcher;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
     Animation animation;
     Animation buttonAnimation;
     ValueAnimator valueAnimator;
+
+    TextSwitcher textSwitcher;
 
     View.OnClickListener handler = new View.OnClickListener() {
         @Override
@@ -38,6 +43,9 @@ public class MainActivity extends AppCompatActivity {
                     break;
                 case R.id.button4:
                     doAction4();
+                    break;
+                case R.id.button5:
+                    doAction5();
                     break;
             }
         }
@@ -64,7 +72,20 @@ public class MainActivity extends AppCompatActivity {
     }
 
     void doAction4(){
-        valueAnimator.start();
+        // xml로 하는거
+//        valueAnimator.start();
+
+        // java로 하기
+        ObjectAnimator animator = ObjectAnimator.ofInt(tv, "backgroundColor", Color.BLACK, Color.RED);
+        animator.setEvaluator(new ArgbEvaluator());
+        animator.setDuration(1000);
+        animator.setRepeatCount(ValueAnimator.INFINITE);
+        animator.setRepeatMode(ValueAnimator.REVERSE);
+        animator.start();
+    }
+
+    void doAction5(){
+        textSwitcher.showNext();
     }
 
     @Override
@@ -98,13 +119,19 @@ public class MainActivity extends AppCompatActivity {
         animation = AnimationUtils.loadAnimation(MainActivity.this, R.anim.move_image);
         buttonAnimation = AnimationUtils.loadAnimation(MainActivity.this, R.anim.button_animation);
 
-        valueAnimator =  (ValueAnimator)AnimatorInflater.loadAnimator(this, R.animator.color_animator); // xml에 있는거 가져오기
-        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener(){
-            @Override
-            public void onAnimationUpdate(ValueAnimator animation) {
-                int color = (Integer)animation.getAnimatedValue();
-                tv.setBackgroundColor(color);
-            }
-        });
+        // xml로 하기
+//        valueAnimator =  (ValueAnimator)AnimatorInflater.loadAnimator(this, R.animator.color_animator); // xml에 있는거 가져오기
+//        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener(){
+//            @Override
+//            public void onAnimationUpdate(ValueAnimator animation) {
+//                int color = (Integer)animation.getAnimatedValue();
+//                tv.setBackgroundColor(color);
+//            }
+//        });
+
+
+        textSwitcher = (TextSwitcher)findViewById(R.id.textSwitcher);
+        findViewById(R.id.button5).setOnClickListener(handler);
+
     }
 }
