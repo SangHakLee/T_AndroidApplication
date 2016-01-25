871f0100b7381543c75017c69a2097cadda6cf75
unknown <sanghaklee>
2016-01-25 09:38:34 +0900

    paint 생성

--- GraphicProject/app/src/main/java/com/example/graphicproject/MyView.java
+++ GraphicProject/app/src/main/java/com/example/graphicproject/MyView.java
@@ -1,7 +1,11 @@
 package com.example.graphicproject;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 /**
@@ -26,8 +30,22 @@ public class MyView extends View {
 
     Context context;
     private void init(Context context){
+        paint = new Paint();
+        paint.setColor(Color.RED);
         this.context = context;
     }
 
+    Paint paint= null; // 그리는 붓 같은 개념
 
+    // 실제 그리는 메소드
+    @Override
+    protected void onDraw(Canvas canvas) {
+//        canvas.drawRect();
+        super.onDraw(canvas);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return super.onTouchEvent(event);
+    }
 }
