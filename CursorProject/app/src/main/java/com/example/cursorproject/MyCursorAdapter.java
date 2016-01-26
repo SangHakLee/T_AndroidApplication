73bfe61cdd9311af0c2bcc6d3096540ed666428b
unknown <sanghaklee>
2016-01-26 11:13:41 +0900

    이미지뷰는 SimpleCursorAdapter 로 불가능 CursorAdapter 로 생성

---
+++ CursorProject/app/src/main/java/com/example/cursorproject/MyCursorAdapter.java
@@ -0,0 +1,30 @@
+package com.example.cursorproject;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+
+/**
+ * Created by skplanet on 2016-01-26.
+ */
+public class MyCursorAdapter extends CursorAdapter {
+    // 메소드 newView bindView 오버라이딩
+    // 생성자 생성
+    
+
+    public MyCursorAdapter(Context context, Cursor c, boolean autoRequery) {
+        super(context, c, autoRequery);
+    }
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        return null;
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+
+    }
+}
