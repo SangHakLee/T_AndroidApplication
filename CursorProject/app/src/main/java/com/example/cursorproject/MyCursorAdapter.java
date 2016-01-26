7bfe7f7be14b42da3a97b6d6d1197629476515d8
unknown <sanghaklee>
2016-01-26 11:24:54 +0900

    cursor 에러

--- CursorProject/app/src/main/java/com/example/cursorproject/MyCursorAdapter.java
+++ CursorProject/app/src/main/java/com/example/cursorproject/MyCursorAdapter.java
@@ -5,6 +5,8 @@ import android.database.Cursor;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * Created by skplanet on 2016-01-26.
@@ -32,6 +34,9 @@ public class MyCursorAdapter extends CursorAdapter {
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
         // newView에서 생선된 view가 넘어온다.
-
+        TextView tv = (TextView)view.findViewById(R.id.textView2);
+        tv.setText(cursor.getString(cursor.getColumnIndex("name") + cursor.getInt(2))); // cursor에서 가져와서 텍스트 뷰에 뿌린다. 이름과 나이 
+        ImageView img = (ImageView)view.findViewById(R.id.imageView);
+        img.setImageResource(imgRes[cursor.getInt(3)]);
     }
 }
