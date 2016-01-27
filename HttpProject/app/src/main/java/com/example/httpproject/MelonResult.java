51e78c4b29fdfa9a0bbb48b364d21a7cfd4eb2d9
unknown <sanghaklee>
2016-01-27 13:29:59 +0900

    ConnectProject init

--- HttpProject/app/src/main/java/com/example/httpproject/MelonResult.java
+++ HttpProject/app/src/main/java/com/example/httpproject/MelonResult.java
@@ -3,9 +3,17 @@ package com.example.httpproject;
 /**
  * Created by skplanet on 2016-01-27.
  * 멜론 정보 최상단에 있는 객체 {}
+ *
+ * {
+ "      melon":{
+ "      menuId":54020101,
+ *
+ *  이런 데이터에서 최상위
+ *  {}  를 위한 객체임
  */
 public class MelonResult {
     public Melon melon;
+
 
     @Override
     public String toString() {
