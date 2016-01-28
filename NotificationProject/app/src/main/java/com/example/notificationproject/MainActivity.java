4adafcc535357f5ce8895c0b7d07bd3d79a4fbfa
unknown <sanghaklee>
2016-01-28 10:19:19 +0900

    노티 개본 패턴

--- NotificationProject/app/src/main/java/com/example/notificationproject/MainActivity.java
+++ NotificationProject/app/src/main/java/com/example/notificationproject/MainActivity.java
@@ -1,14 +1,17 @@
 package com.example.notificationproject;
 
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.os.Bundle;
 import android.os.Vibrator;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "Main";
@@ -20,15 +23,27 @@ public class MainActivity extends AppCompatActivity {
                 case R.id.button:
                     doAction1();
                     break;
+                case R.id.button2:
+                    doAction2();
+                    break;
             }
         }
     };
 
-    // 진동관리 객체
-    Vibrator vibrator;
+    NotificationManager manager; // 노티 관리 객체
+    //노티
+    public void doAction2(){
 
+        // 노티 기본 패턴
+        Notification notification = new NotificationCompat.Builder(this).build();
+        manager.notify(1, notification);
+    }
+
+    Vibrator vibrator;     // 진동관리 객체
     public void doAction1(){
         vibrator.vibrate(2000); // 2초 동안 진동
+        long[] pattern ={1000, 500, 200, 400, 1000}; // 패턴으로 진동이 온다.
+        vibrator.vibrate(pattern, 1);
     }
 
     @Override
@@ -37,8 +52,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         findViewById(R.id.button).setOnClickListener(handler);
+        findViewById(R.id.button2).setOnClickListener(handler);
 
+        // getSystemService 에서 얻는건 한번만 얻으면 되는거
         vibrator = (Vibrator)getSystemService(VIBRATOR_SERVICE); // 하드웨서 진동 객체 얻기
+        manager = (NotificationManager)getSystemService(NOTIFICATION_SERVICE); // 노티 객체 얻기
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
