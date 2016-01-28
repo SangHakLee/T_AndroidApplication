1b7638ed61d13e8453e8f32917a15459fdc11ea7
unknown <sanghaklee>
2016-01-28 14:36:34 +0900

    방송 리시버에서 받기 onReceive

--- LocationProject/app/src/main/java/com/example/locationproject/MyReceiver.java
+++ LocationProject/app/src/main/java/com/example/locationproject/MyReceiver.java
@@ -3,15 +3,19 @@ package com.example.locationproject;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.location.LocationManager;
+import android.util.Log;
 
 public class MyReceiver extends BroadcastReceiver {
+    private static final String TAG = "Main";
+
     public MyReceiver() {
     }
 
+    // Main에서 보낸 intent가 여기로 들어온다
     @Override
     public void onReceive(Context context, Intent intent) {
-        // TODO: This method is called when the BroadcastReceiver is receiving
-        // an Intent broadcast.
-        throw new UnsupportedOperationException("Not yet implemented");
+        boolean flag = intent.getBooleanExtra(LocationManager.KEY_PROXIMITY_ENTERING, true); // Main에서 준거 받아오기
+        Log.v(TAG, flag ? "영역안에 들어옴" : "영역에서 벗어남");
     }
 }
