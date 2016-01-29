97447b1912a57aeb61ddebc3e59a0428c1eb975c
unknown <sanghaklee>
2016-01-29 16:59:56 +0900

    앱 위젯 클릭하면 뷰 변화

--- AppWidgetProject/app/src/main/java/com/example/appwidgetproject/MyAppWidgetProvider.java
+++ AppWidgetProject/app/src/main/java/com/example/appwidgetproject/MyAppWidgetProvider.java
@@ -1,8 +1,11 @@
 package com.example.appwidgetproject;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
 import android.util.Log;
 import android.widget.RemoteViews;
 
@@ -13,14 +16,46 @@ public class MyAppWidgetProvider extends AppWidgetProvider {
     private static final String TAG = "Main";
     int cnt = 0;
 
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+
+        String action = intent.getAction();
+        if("com.example.appwidgetproject.COUNT".equals(action)){
+            doProcess(context, intent);
+        }
+        doProcess(context, intent);
+    }
+
+    void doProcess(Context context, Intent intent){
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.my_app_widget_provider);
+        views.setTextViewText(R.id.textView, intent.getStringExtra("cnt"));
+
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+
+        ComponentName componentName = new ComponentName(context, MyAppWidgetProvider.class);
+
+        appWidgetManager.updateAppWidget(componentName, views); // UI 바뀌주는 메소드 updateAppWidget()
+
+
+    };
+
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
-        Log.v(TAG, "updateAppWidget : "+ appWidgetId);
+        Log.v(TAG, "updateAppWidget : " + appWidgetId);
         CharSequence widgetText = context.getString(R.string.appwidget_text);
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.my_app_widget_provider);
         views.setTextViewText(R.id.textView, widgetText);
+
+        Intent intent = new Intent("com.example.appwidgetproject.COUNT"); // 펜딩에 걸 인텐트
+        intent.putExtra("cnt", "123");
+
+        // PendingIntent.FLAG_CANCEL_CURRENT 현재 액티비티 종료
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT);// intent 방송을 뿌린다.
+        views.setOnClickPendingIntent(R.id.button, pendingIntent); // 버튼 클릭시 펜딩 인텐트
+
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
@@ -42,6 +77,12 @@ public class MyAppWidgetProvider extends AppWidgetProvider {
         cnt++;
         Log.v(TAG, "onEnabled cnt : "+cnt);
         // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDeleted(Context context, int[] appWidgetIds) {
+        Log.v(TAG, "onDeleted cnt : "+cnt);
+        super.onDeleted(context, appWidgetIds);
     }
 
     @Override
