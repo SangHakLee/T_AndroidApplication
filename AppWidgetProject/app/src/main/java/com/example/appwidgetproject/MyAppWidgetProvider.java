394e5bd8c10c690e9fb2c3d2bc5cd2373cd3e9b4
unknown <sanghaklee>
2016-01-29 16:30:08 +0900

    뷰 만들고 라이프사이클 체크를 위한 로그 추

--- AppWidgetProject/app/src/main/java/com/example/appwidgetproject/MyAppWidgetProvider.java
+++ AppWidgetProject/app/src/main/java/com/example/appwidgetproject/MyAppWidgetProvider.java
@@ -3,20 +3,24 @@ package com.example.appwidgetproject;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.util.Log;
 import android.widget.RemoteViews;
 
 /**
  * Implementation of App Widget functionality.
  */
 public class MyAppWidgetProvider extends AppWidgetProvider {
+    private static final String TAG = "Main";
+    int cnt = 0;
 
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
+        Log.v(TAG, "updateAppWidget : "+ appWidgetId);
         CharSequence widgetText = context.getString(R.string.appwidget_text);
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.my_app_widget_provider);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
+        views.setTextViewText(R.id.textView, widgetText);
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
@@ -25,6 +29,9 @@ public class MyAppWidgetProvider extends AppWidgetProvider {
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         // There may be multiple widgets active, so update all of them
+        cnt++;
+        Log.v(TAG, "onUpdate cnt : "+cnt);
+
         for (int appWidgetId : appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId);
         }
@@ -32,11 +39,15 @@ public class MyAppWidgetProvider extends AppWidgetProvider {
 
     @Override
     public void onEnabled(Context context) {
+        cnt++;
+        Log.v(TAG, "onEnabled cnt : "+cnt);
         // Enter relevant functionality for when the first widget is created
     }
 
     @Override
     public void onDisabled(Context context) {
+        cnt++;
+        Log.v(TAG, "onDisabled cnt : "+cnt);
         // Enter relevant functionality for when the last widget is disabled
     }
 }
